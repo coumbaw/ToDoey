4b696ce7bedc9112da6859c50ee63186ba49caca
Coumba Evelyn Winfield <coumbaw@icloud.com>
2018-02-20 20:44:20 -0500

    Todo list complete with datasource and Delegate Message

---
+++ ToDoey/TodoListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Coumba Winfield on 2/20/18.
+//  Copyright © 2018 Favor it LLC. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+	let itemArray = ["Find Schmakums", "Make Schmakums", "Eat Schmakums"]
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	//MARK - Tableview Datasource Methods
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return itemArray.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+	
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+	
+		cell.textLabel?.text = itemArray[indexPath.row]
+		
+		return cell
+	}
+	
+	//MARK - TableView Delegate Methods
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//print("I was pressed! " + itemArray[indexPath.row])
+		
+		if 		tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .none
+		}
+		else {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+
+		}
+
+		tableView.deselectRow(at: indexPath, animated: true)
+		
+			
+
+	}
+
+}
+
